38240fc09cb480d47109647c590c27d83155ba37
podhmo <ababjam61+github@gmail.com>
2017-01-03 21:38:46 +0900

    example

---
+++ examples/00connexion-example/readme.md
@@ -0,0 +1,6 @@
+from [connexion example](https://github.com/hjacobs/connexion-example)
+
+generated code:
+
+- [./app/routes.py](./app/routes.py)
+- [./app/views.py](./app/views.py)
--- examples/00simple/app/routes.py
+++
@@ -1,8 +0,0 @@
-def includeme_swagger_router(config):
-    config.add_route('pets', '/api/pets')
-    config.add_route('pet', '/api/pets/{pet_id}')
-    config.scan('.views')
-
-
-def includeme(config):
-    config.include(includeme_swagger_router)
